5bacca675456ce986d4e6794aa175c5bffdf5361
Ryan Forsyth <ryanforsyth@gmail.com>
2012-12-30 17:24:27 -0800

    trying to fix exception bug created by deprecated read_inheritable_attribute

--- lib/exceptions/exception_loggable.rb
+++ lib/exceptions/exception_loggable.rb
@@ -26,15 +26,20 @@ module ExceptionLogger
     end
   
     module ClassMethods
+      
+      class_attribute :local_addresses
+      class_attribute :exception_data
+      
       def consider_local(*args)
         local_addresses.concat(args.flatten.map { |a| IPAddr.new(a) })
       end
 
       def local_addresses
-        addresses = read_inheritable_attribute(:local_addresses)
-        unless addresses
+        # addresses = read_inheritable_attribute(:local_addresses)
+        unless local_addresses
           addresses = [IPAddr.new("127.0.0.1")]
-          write_inheritable_attribute(:local_addresses, addresses)
+          # write_inheritable_attribute(:local_addresses, addresses)
+          local_addresses = addresses
         end
         addresses
       end
@@ -42,9 +47,11 @@ module ExceptionLogger
       def exception_data(deliverer = self, &block)
         deliverer = block if block
         if deliverer == self
-          read_inheritable_attribute(:exception_data)
+          # read_inheritable_attribute(:exception_data)
+          exception_data
         else
-          write_inheritable_attribute(:exception_data, deliverer)
+          # write_inheritable_attribute(:exception_data, deliverer)
+          exception_data = deliverer
         end
       end
     end
