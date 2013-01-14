7edf3957325ce5758b0157570eeeaef00d4bb726
Ryan Forsyth <ryanforsyth@gmail.com>
2013-01-13 16:34:55 -0800

    removing deprecated startup attributes

--- lib/api/api_configuration.rb
+++ lib/api/api_configuration.rb
@@ -2,15 +2,12 @@
 module Api
 	
   class ApiConfiguration
-    attr_reader :protocol, :supports_ssl
-    attr_accessor :message_title, :message_detail, :allow_access,
-                  :use_www_subdomain_for_ssl
+    attr_reader :supports_ssl
+    attr_accessor :message_title, :message_detail, :allow_access
     
-    def initialize(protocol, supports_ssl)
-      @protocol = protocol
+    def initialize(supports_ssl)
       @supports_ssl = supports_ssl
       @allow_access = 1
-      @use_www_subdomain_for_ssl = 0
     end
   end
 	
