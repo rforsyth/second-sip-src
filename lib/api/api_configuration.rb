700ada900350b4b630d407570d9384c822342e78
Ryan Forsyth <ryanforsyth@gmail.com>
2013-01-14 20:11:32 -0800

    adding the use_api_subdomain flag

--- lib/api/api_configuration.rb
+++ lib/api/api_configuration.rb
@@ -3,11 +3,12 @@ module Api
 	
   class ApiConfiguration
     attr_reader :supports_ssl
-    attr_accessor :message_title, :message_detail, :allow_access
+    attr_accessor :message_title, :message_detail, :allow_access, :use_api_subdomain
     
     def initialize(supports_ssl)
       @supports_ssl = supports_ssl
       @allow_access = 1
+      @use_api_subdomain = 0
     end
   end
 	
