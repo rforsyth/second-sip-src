2961cb89013087b55c72916ff838a1243e4083d1
Ryan Forsyth <ryanforsyth@gmail.com>
2013-01-14 20:48:52 -0800

    turning off https and using api domain - just testing

--- app/controllers/api_startup_controller.rb
+++ app/controllers/api_startup_controller.rb
@@ -4,7 +4,7 @@ require 'api/api_configuration'
 class ApiStartupController < ApiController
   
   def configuration
-    configuration = Api::ApiConfiguration.new(1)
+    configuration = Api::ApiConfiguration.new(0)
     #configuration.message_title = 'Got Yer Message'
     #configuration.message_detail = 'Hello Startup!'
     configuration.allow_access = 1
